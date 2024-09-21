c14f2847b8f1704921f43410c0881fe016d71095
Eric Skaftason <skaftasoneric@gmail.com>
2024-09-21 17:02:07 -0400

    added port param for excecution

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const main = require('./logic/main');  // Import logic from main.js
 
-const port = 3000;
+const port = process.argv[2];
 
-// Middleware to parse JSON bodies
+if (!port || isNaN(port) || port <= 0 || port > 65535) {
+    console.error('Invalid or missing port number. Please enter a valid number between 1 and 65535.');
+} else {
+    console.log(`Server will start on port ${port}.`);
+}
+
+
+// Middleware to parse JSON bodiese
 app.use(bodyParser.json());
 
 // Serve static files from the 'public' directory
